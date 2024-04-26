a84fb6230d2fe9142ee7f060f5e4e9c848dcdde4
abhi21121211 <111838547+abhi21121211@users.noreply.github.com>
2024-04-26 22:45:24 +0530

    add api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,169 @@
-import logo from './logo.svg';
+
+import { Oval } from 'react-loader-spinner';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFrown } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
-function App() {
+function WeatherApp() {
+  const [input, setInput] = useState('');
+  const [weather, setWeather] = useState({
+    loading: false,
+    data: {},
+    error: false,
+  });
+
+  const toDateFunction = () => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    const WeekDays = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+    const currentDate = new Date();
+    const date = `${WeekDays[currentDate.getDay()]} ${currentDate.getDate()} ${months[currentDate.getMonth()]
+      }`;
+    return date;
+  };
+
+  const search = async (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      setInput('');
+      setWeather({ ...weather, loading: true });
+      const url = 'https://api.openweathermap.org/data/2.5/weather';
+      const api_key = 'f00c38e0279b7bc85480c3fe775d518c';
+      await axios
+        .get(url, {
+          params: {
+            q: input,
+            units: 'metric',
+            appid: api_key,
+          },
+        })
+        .then((res) => {
+          console.log('res', res);
+          setWeather({ data: res.data, loading: false, error: false });
+        })
+        .catch((error) => {
+          setWeather({ ...weather, data: {}, error: true });
+          setInput('');
+          console.log('error', error);
+        });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="card">
+        <svg height="100%" width="100%" class="border" xmlns="http://www.w3.org/2000/svg">
+          <line y2="0" x2="600" y1="0" x1="0" class="top"></line>
+          <line y2="-960" x2="0" y1="320" x1="0" class="left"></line>
+          <line y2="320" x2="-600" y1="320" x1="200" class="bottom"></line>
+          <line y2="960" x2="200" y1="0" x1="200" class="right"></line>
+        </svg>
+        
+      </div>
+
+      
+
+      <h1 className="app-name">
+        <button className="button">
+          Check My Current Weather
+          <span></span>
+          <span></span>
+        </button>
+      </h1>
+      <div className="container">
+        <div className="search-container">
+          <input className="input" type="text"
+            placeholder="Enter City Name.."
+            name="query"
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+            onKeyPress={search}
+          />
+            <svg viewBox="0 0 24 24" className="search__icon">
+              <g>
+                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
+                </path>
+              </g>
+            </svg>
+        </div>
+      </div>
+      {/* <div className="search-bar">
+        <input
+          type="text"
+          className="city-search"
+          placeholder="Enter City Name.."
+          name="query"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+          onKeyPress={search}
+        />
+      </div> */}
+      {weather.loading && (
+        <>
+          <br />
+          <br />
+          <Oval type="Oval" color="black" height={100} width={100} />
+        </>
+      )}
+      {weather.error && (
+        <>
+          <br />
+          <br />
+          <span className="error-message">
+            <FontAwesomeIcon icon={faFrown} />
+            <span style={{ fontSize: '20px' }}>City not found</span>
+          </span>
+        </>
+      )}
+      {weather && weather.data && weather.data.main && (
+        <div>
+          <div className="city-name">
+            <h2>
+              {weather.data.name}, <span>{weather.data.sys.country}</span>
+            </h2>
+          </div>
+          <div className="date">
+            <span>{toDateFunction()}</span>
+          </div>
+          <div className="icon-temp">
+            <img
+              className=""
+              src={`https://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`}
+              alt={weather.data.weather[0].description}
+            />
+            {Math.round(weather.data.main.temp)}
+            <sup className="deg">°C</sup>
+          </div>
+          <div className="des-wind">
+            <p>{weather.data.weather[0].description.toUpperCase()}</p>
+            <p>Wind Speed: {weather.data.wind.speed}m/s</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default WeatherApp;
