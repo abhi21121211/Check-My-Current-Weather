92ea20d0f32c1f8c9fb6203e4ef2900993c0ed37
abhi21121211 <111838547+abhi21121211@users.noreply.github.com>
2024-04-27 14:35:27 +0530

    Search  btn work done

--- src/App.js
+++ src/App.js
@@ -45,9 +45,9 @@ function WeatherApp() {
   };
 
   const search = async (event) => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      setInput('');
+    // if (event.key === 'Enter') {
+    //   event.preventDefault();
+    //   setInput('');
       setWeather({ ...weather, loading: true });
       const url = 'https://api.openweathermap.org/data/2.5/weather';
       const api_key = 'f00c38e0279b7bc85480c3fe775d518c';
@@ -68,7 +68,7 @@ function WeatherApp() {
           setInput('');
           console.log('error', error);
         });
-    }
+    // }
   };
 
   return (
@@ -94,32 +94,29 @@ function WeatherApp() {
       </h1>
       <div className="container">
         <div className="search-container">
-          <input className="input" type="text"
+          <input
+            className="input"
+            type="text"
             placeholder="Enter City Name.."
             name="query"
             value={input}
             onChange={(event) => setInput(event.target.value)}
-            onKeyPress={search}
+            
+            onKeyPress={(event) => {
+              if (event.key === 'Enter') {
+                search();
+              }
+            }}
           />
-            <svg viewBox="0 0 24 24" className="search__icon">
-              <g>
-                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
-                </path>
-              </g>
-            </svg>
+          <svg viewBox="0 0 24 24" className="search__icon" onClick={search}>
+            <g >
+              <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+            </g>
+          </svg>
         </div>
       </div>
-      {/* <div className="search-bar">
-        <input
-          type="text"
-          className="city-search"
-          placeholder="Enter City Name.."
-          name="query"
-          value={input}
-          onChange={(event) => setInput(event.target.value)}
-          onKeyPress={search}
-        />
-      </div> */}
+
+     
       {weather.loading && (
         <>
           <br />
